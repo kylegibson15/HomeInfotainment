d7dc261e957a4e587ba947ccd42ae98885c09371
kylegibson15 <kylegibson15@gmail.com>
2020-10-27 21:54:37 -0600

    Updates

---
+++ precache-manifest.aac902de3bfcd2ea9b7230717309af36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48fe0239b302e925aa0048dcbff8ec16",
+    "url": "/HomeInfotainment/index.html"
+  },
+  {
+    "revision": "1cdc714224cf48823ee9",
+    "url": "/HomeInfotainment/static/css/main.6c5cc8a7.chunk.css"
+  },
+  {
+    "revision": "da40b28ab2b302e11db1",
+    "url": "/HomeInfotainment/static/js/2.2b834d5b.chunk.js"
+  },
+  {
+    "revision": "0d08bb566150086a6317785a01bb3007",
+    "url": "/HomeInfotainment/static/js/2.2b834d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cdc714224cf48823ee9",
+    "url": "/HomeInfotainment/static/js/main.32379a16.chunk.js"
+  },
+  {
+    "revision": "aa2a260e9879a83554ff",
+    "url": "/HomeInfotainment/static/js/runtime-main.7872ba4a.js"
+  }
+]);
